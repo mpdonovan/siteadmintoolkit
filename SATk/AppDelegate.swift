45979ec5b7cc456c746739c46645ea2ffe80ef70
Mike Donovan <mike.donovan@killeenisd.org>
2017-12-16 12:39:58 -0600

    Initial Commit

---
+++ SATk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SATk
+//
+//  Created by Donovan, Mike on 12/16/17.
+//  Copyright © 2017 Donovan, Mike. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
